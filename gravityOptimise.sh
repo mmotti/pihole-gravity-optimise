ae63791f0e5ae357b8e18c41d651575430d69c44
MMotti <matthew.w.motti@gmail.com>
2018-08-29 19:50:19 +0100

    Process dnsmasq wildcards first This could save valuable regexp time, as processing the fixed-string dnsmasq wildcards is faster.
    With my setup, running the wildcard replacement first (from my filter-lists.conf file), there are 54,873 less lines to run regexps against.

--- gravityOptimise.sh
+++ gravityOptimise.sh
@@ -17,55 +17,6 @@ pihole_update ()
 	echo "--> $count_gravity gravity list entries"
 }
 
-process_regex ()
-{
-	echo "#### Regex Removals ####"
-
-	# Check gravity.list is not empty
-        if [ ! -s $file_gravity ]; then
-                echo "--> gravity.list is empty or does not exist"
-                return 1
-        fi
-
-        # Count gravity entries
-        count_gravity=$(wc -l < $file_gravity)
-
-	# Only read it if it exists and is not empty
-	if [ -s $file_regex ]; then
-		regexList=$(grep '^[^#]' $file_regex)
-	else
-		echo "--> Regex list is empty or does not exist."
-		return 1
-	fi
-
-	# Status update
-	echo "--> $(wc -l <<< "$regexList") regexps found"
-
-	echo "--> Identifying unnecessary domains (this may take a while)"
-
-	# For each regex entry
-	# Add any regex matches to an array
-
-	new_gravity=$(grep -vEf <(echo "$regexList") $file_gravity)
-
-	if [ -z "$new_gravity" ]; then
-		echo "--> No unnecessary domains were found"
-		return 0
-	fi
-
-	# Status update
-	echo "--> $(($count_gravity-$(wc -l <<< "$new_gravity"))) unnecessary hosts identified"
-
-	# Output file
-	echo "--> Outputting $file_gravity"
-	echo "$new_gravity" | sudo tee $file_gravity > /dev/null
-
-	# Status update
-	echo "--> $(wc -l < $file_gravity) domains in gravity.list"
-
-	return 0
-}
-
 process_wildcards () {
 
 	echo "#### Wildcard Removals ####"
@@ -140,6 +91,55 @@ process_wildcards () {
 	return 0
 }
 
+process_regex ()
+{
+	echo "#### Regex Removals ####"
+
+	# Check gravity.list is not empty
+        if [ ! -s $file_gravity ]; then
+                echo "--> gravity.list is empty or does not exist"
+                return 1
+        fi
+
+        # Count gravity entries
+        count_gravity=$(wc -l < $file_gravity)
+
+	# Only read it if it exists and is not empty
+	if [ -s $file_regex ]; then
+		regexList=$(grep '^[^#]' $file_regex)
+	else
+		echo "--> Regex list is empty or does not exist."
+		return 1
+	fi
+
+	# Status update
+	echo "--> $(wc -l <<< "$regexList") regexps found"
+
+	echo "--> Identifying unnecessary domains (this may take a while)"
+
+	# For each regex entry
+	# Add any regex matches to an array
+
+	new_gravity=$(grep -vEf <(echo "$regexList") $file_gravity)
+
+	if [ -z "$new_gravity" ]; then
+		echo "--> No unnecessary domains were found"
+		return 0
+	fi
+
+	# Status update
+	echo "--> $(($count_gravity-$(wc -l <<< "$new_gravity"))) unnecessary hosts identified"
+
+	# Output file
+	echo "--> Outputting $file_gravity"
+	echo "$new_gravity" | sudo tee $file_gravity > /dev/null
+
+	# Status update
+	echo "--> $(wc -l < $file_gravity) domains in gravity.list"
+
+	return 0
+}
+
 finalise () {
 
 	echo "#### Finalise changes ####"
@@ -151,9 +151,9 @@ finalise () {
 
 # Run gravity update
 pihole_update
-# Process regex removals
-process_regex
 # Process wildcard removals
 process_wildcards
+# Process regex removals
+process_regex
 # Finish up
 finalise
