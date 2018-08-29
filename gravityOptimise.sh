5110f8c58d6a579281234e6a733b1ea0b1e603c2
MMotti <matthew.w.motti@gmail.com>
2018-08-29 16:17:09 +0100

    Minor changes

--- gravityOptimise.sh
+++ gravityOptimise.sh
@@ -8,18 +8,31 @@ file_regex="/etc/pihole/regex.list"
 pihole_update ()
 {
 	echo "#### Gravity List ####"
+
 	echo "--> Updating gravity.list"
 	pihole updateGravity > /dev/null
-	echo "--> $(wc -l < $file_gravity) gravity list entries"
+	# Count gravity entries
+	count_gravity=$(wc -l < $file_gravity)
+	# Status update
+	echo "--> $count_gravity gravity list entries"
 }
 
 process_regex ()
 {
 	echo "#### Regex Removals ####"
 
+	# Check gravity.list is not empty
+        if [ ! -s $file_gravity ]; then
+                echo "--> gravity.list is empty or does not exist"
+                return 1
+        fi
+
+        # Count gravity entries
+        count_gravity=$(wc -l < $file_gravity)
+
 	# Only read it if it exists and is not empty
 	if [ -s $file_regex ]; then
-		regexList=$(sort -u $file_regex)
+		regexList=$(grep '^[^#]' $file_regex)
 	else
 		echo "--> Regex list is empty or does not exist."
 		return 1
@@ -28,36 +41,27 @@ process_regex ()
 	# Status update
 	echo "--> $(wc -l <<< "$regexList") regexps found"
 
-	# Check gravity.list is not empty
-	if [ ! -s $file_gravity ]; then
-		echo "--> gravity.list is empty or does not exist"
-		return 1
-	fi
-
-	# Status update
-	echo "--> $(wc -l < $file_gravity) gravity.list entries"
-
-	echo "--> Identifying unnecessary domains"
+	echo "--> Identifying unnecessary domains (this may take a while)"
 
 	# For each regex entry
 	# Add any regex matches to an array
 
 	new_gravity=$(grep -vEf <(echo "$regexList") $file_gravity)
 
-	if [ -z "$new_gravity" ] || [ $(wc -l <<< "$new_gravity") = 0 ]; then
+	if [ -z "$new_gravity" ]; then
 		echo "--> No unnecessary domains were found"
 		return 0
 	fi
 
 	# Status update
-	echo "--> $(($(wc -l < $file_gravity)-$(wc -l <<< "$new_gravity"))) unnecessary hosts identified"
+	echo "--> $(($count_gravity-$(wc -l <<< "$new_gravity"))) unnecessary hosts identified"
 
 	# Output file
 	echo "--> Outputting $file_gravity"
 	echo "$new_gravity" | sudo tee $file_gravity > /dev/null
 
 	# Status update
-	echo "--> $(wc -l <<< "$new_gravity") domains in gravity.list"
+	echo "--> $(wc -l < $file_gravity) domains in gravity.list"
 
 	return 0
 }
@@ -66,32 +70,34 @@ process_wildcards () {
 
 	echo "#### Wildcard Removals ####"
 
+
+	# Check gravity.list is not empty
+        if [ ! -s $file_gravity ]; then
+                echo "--> gravity.list is empty or does not exist"
+                return 1
+        fi
+
+	# Fetch initial gravity count
+	count_gravity=$(wc -l < $file_gravity)
+
 	# Grab unique base domains from dnsmasq conf files
-        echo "--> Fetching domains from $dir_wildcards"
+        echo "--> Fetching wildcards from $dir_wildcards"
 	domains=$(find $dir_wildcards -name "*.conf" -type f -print0 |
-		xargs -r0 grep -hE "^address=\/.+\/(0\.0\.0\.0|::)?$" |
-			awk -F'/' '{print $2}' |
+		xargs -r0 grep -hE "^address=\/.+\/(([0-9]\.){3}[0-9]|::)?$" |
+			cut -d'/' -f2 |
 				sort -u)
 
-	 # Conditional exit
+	# Conditional exit
         if [ -z "$domains" ]; then
                 echo "--> No wildcards were captured from $dir_wildcards"
                 return 1
         fi
 
-        echo "--> $(wc -l <<< "$domains") wildcards in $dir_wildcards"
+        echo "--> $(wc -l <<< "$domains") wildcards found"
 
 	# Add ^ prefix and $ suffix to gravity (for comparison)
 	echo "--> Processing $file_gravity"
 	gravity_ps=$(sed 's/^/\^/g;s/$/\$/g' $file_gravity)
-
-	# Conditional exit
-	if [ -z "$gravity_ps" ]; then
-		echo "--> There is an issue with gravity.list"
-		return 1
-	fi
-
-	echo "--> $(wc -l <<< "$gravity_ps") domains in gravity.list"
 
 	# Convert something.com to .something.com$
 	# Convert something.com to ^something.com$
@@ -112,11 +118,11 @@ process_wildcards () {
 	sort)
 
 	# Status update
-        removal_count=$(($(wc -l <<< "$gravity_ps")-$(wc -l <<< "$new_gravity")))
+        removal_count=$(($count_gravity-$(wc -l <<< "$new_gravity")))
 
 	# If there was an error populating new_gravity.list
-	# Or there are no changes to be made
-	if [ -z "$new_gravity" ] || [ $removal_count = 0 ]; then
+	# Or no changes need to be made
+	if [ -z "$new_gravity" ] || [ "$removal_count" = 0 ]; then
 		echo "--> No changes required."
 		return 0
 	fi
@@ -129,7 +135,7 @@ process_wildcards () {
 	echo "$new_gravity" | sudo tee $file_gravity > /dev/null
 
 	# Status update
-        echo "--> $(wc -l <<< "$new_gravity") domains in gravity.list"
+        echo "--> $(wc -l < $file_gravity) domains in gravity.list"
 
 	return 0
 }
