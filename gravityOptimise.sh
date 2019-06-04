1184c59b96a9c6cfca6e69a9d6131c09dbec8953
MMotti <matthew.w.motti@gmail.com>
2019-06-04 02:07:24 +0100

    Update to accommodate for Pi-hole DB It was necessary to update the script to work with Pi-hole db. This is currently only applicable to the development branch but the db will be rolled out to master in future.

--- gravityOptimise.sh
+++ gravityOptimise.sh
@@ -1,55 +1,134 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC2034  # Unused variables left for readability
 
 # Set file variables
-file_gravity='/etc/pihole/gravity.list'
+db_gravity='/etc/pihole/gravity.db'
 dir_dnsmasq='/etc/dnsmasq.d'
-file_regex='/etc/pihole/regex.list'
+file_gravity_tmp='/etc/pihole/gravity.list.tmp'
+
+# Functions
+function fetchTable {
+
+	local table queryStr
+
+	table="${1}"
+	queryStr="Select domain FROM vw_${table};"
+
+	sqlite3 ${db_gravity} "${queryStr}"
+
+	return
+}
+
+function updateGravity {
+
+	# Code here is adapted from /opt/pihole/gravity.sh
+
+	local file_gravity_tmp table
+
+	file_gravity_tmp="${1}"
+	table='gravity'
+
+	# Another conditional exit
+	if [[ ! -e "${file_gravity_tmp}" ]] || [[ ! -s "${file_gravity_tmp}" ]]; then
+		echo "[i] Unable to process ${file_gravity_tmp}"
+		exit 1
+	fi
+
+	# Truncate gravity table
+	output=$( { sudo sqlite3 ${db_gravity} "DELETE FROM ${table}"; } 2>&1 )
+	status="$?"
+
+	[[ "${status}" -ne  0 ]] && echo '[i] Failed to truncate gravity DB' && exit 1
+
+	# Upload optimised gravity
+	output=$( { printf ".mode csv\\n.import \"%s\" %s\\n" "${file_gravity_tmp}" "${table}" | sudo sqlite3 "${db_gravity}"; } 2>&1 )
+	status="$?"
+
+	[[ "${status}" -ne  0 ]] && echo '[i] Unable to load entries into gravity' && exit 1
+
+	# Remove temp gravity file
+	sudo rm -f "${file_gravity_tmp}"
+
+	return
+}
+
+# Conditional exit
+if [[ ! -e "${db_gravity}" ]] || [[ ! -s "${db_gravity}" ]]; then
+	echo '[i] You have not yet migrated to the pi-hole database. Please run the old script.'; exit 1
+fi
 
 # Update gravity.list
-echo "[i] Updating gravity.list"
+echo '[i] Updating gravity.list'
 pihole updateGravity > /dev/null
-num_gravity_before=$(wc -l < $file_gravity)
 
-# Conditional exit if gravity.list is empty
-[ ! -s "$file_gravity" ] && echo 'gravity.list is empty / not found' && exit
+# Save gravity to temp file
+str_gravity=$(fetchTable "gravity")
+
+# Output if result returned
+if [[ -n "${str_gravity}" ]]; then
+	echo "${str_gravity}" | sudo tee "${file_gravity_tmp}" > /dev/null
+else
+	echo '[i] No results returned from gravity db'; exit 1;
+fi
+
+# Save regex in str
+str_regex=$(fetchTable "regex")
+
+# Grab gravity count pre-processing
+num_gravity_before=$(wc -l < "$file_gravity_tmp")
 
 # Identify existing local wildcards
 echo '[i] Parsing existing wildcard config (DNSMASQ)'
-existing_wildcards=$(find $dir_dnsmasq -type f -name '*.conf' -print0 |
+existing_wildcards=$(find "${dir_dnsmasq}" -type f -name '*.conf' -print0 |
 	xargs -r0 grep -hE '^address=\/.+\/(([0-9]{1,3}\.){3}[0-9]{1,3}|::|#)?$' |
 		cut -d '/' -f2 |
 			sort -u)
 
-if [ -n "$existing_wildcards" ]; then
+if [[ -n "${existing_wildcards}" ]]; then
 	# If there are existing wildcards
 	echo '[i] Removing wildcard matches in gravity.list'
 	# Convert exact domains (pattern source) - something.com -> ^something.com$
-	match_exact=$(sed 's/^/\^/;s/$/\$/' <<< "$existing_wildcards")
+	match_exact=$(sed 's/^/\^/;s/$/\$/' <<< "${existing_wildcards}")
 	# Convert wildcard domains (pattern source) - something.com - .something.com$
-	match_wildcard=$(sed 's/^/\./;s/$/\$/' <<< "$existing_wildcards")
+	match_wildcard=$(sed 's/^/\./;s/$/\$/' <<< "${existing_wildcards}")
 	# Convert target - something.com -> ^something.com$
-	match_target=$(sed 's/^/\^/;s/$/\$/' <(cat $file_gravity))
+	match_target=$(sed 's/^/\^/;s/$/\$/' "${file_gravity_tmp}")
 	# Compile the exact and wildcard match patterns
-	match_patterns=$(printf '%s\n' "$match_exact" "$match_wildcard")
-	# Invert match patterns	
-	new_gravity=$(grep -vFf <(echo "$match_patterns") <<< "$match_target" | sed 's/[\^$]//g')
-	# Output to gravity.list
-	echo "$new_gravity" | sudo tee $file_gravity > /dev/null
+	match_patterns=$(printf '%s\n' "${match_exact}" "${match_wildcard}")
+	# Invert match patterns
+	str_gravity=$(grep -vFf <(echo "${match_patterns}") <<< "${match_target}" | sed 's/[\^$]//g')
+
+	# Conditional exit
+	if [[ -n "${str_gravity}" ]]; then
+		echo "${str_gravity}" | sudo tee "${file_gravity_tmp}" > /dev/null
+	else
+		echo '[i] 0 domains remain after wildcard removals'; exit 0;
+	fi
 fi
 
-if [ -s "$file_regex" ]; then
+if [[ -n "${str_regex}" ]]; then
 	echo '[i] Removing regex.list matches'
 	# Remove comments from regex file
-	regexps=$(grep '^[^#]' $file_regex)
+	regexps=$(grep '^[^#]' <<< "${str_regex}")
 	# Invert match regex.list
-	new_gravity=$(grep -vEf <(echo "$regexps") $file_gravity)
-	# Output to gravity.list
-	echo "$new_gravity" | sudo tee $file_gravity > /dev/null
+	str_gravity=$(grep -vEf <(echo "${regexps}") <(echo "{$str_gravity}"))
+
+	# Conditional exit
+	if [[ -n "${str_gravity}" ]]; then
+		echo "${str_gravity}" | sudo tee "${file_gravity_tmp}" > /dev/null
+	else
+		echo '[i] 0 domains remain after regex removals'; exit 0;
+	fi
 fi
 
+# Update gravity DB
+echo '[i] Updating gravity table'
+updateGravity "${file_gravity_tmp}"
+
 # Some status
-num_gravity_after=$(wc -l < $file_gravity)
-echo "[i] $((num_gravity_before-num_gravity_after)) gravity.list entries removed"
+str_gravity=$(fetchTable "gravity")
+num_gravity_after=$(wc -l <<< "${str_gravity}")
+echo "[i] $((num_gravity_before-num_gravity_after)) gravity entries removed"
 
 # Refresh pi-hole
 echo "[i] Sending SIGHUP to Pihole"
